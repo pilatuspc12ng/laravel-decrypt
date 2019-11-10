00a2b06e2db88eefa2dfc85f07aa3a52a28eac54
Rob Goodwin <rob@goodmail.us>
2019-11-09 19:43:34 -0600

    * Added key as input to decrypt() function. Key must be base64 decoded. * Fixed bug in vald_mac() function where data was passed in as input instead of payload. *decrypt() function will now return a decoded UTF-8 string instead of a byte string.

--- crypt.py
+++ crypt.py
@@ -1,4 +1,3 @@
-# Python3 code below
 import os
 import base64
 import json
@@ -8,29 +7,51 @@ import hashlib
 import hmac
 
 
-def decrypt(payload):
+def decrypt(payload, key):
+    """
+    Decrypt strings that have been encrypted using Laravel's encrypter (AES-256 encryption).
+
+    Plain text is encrypted in Laravvel using the following code:
+    >>> ciphertext = Crypt::encrypt('hello world');
+
+    The ciphertext is a base64's json-encoded array consisting of the following keys:
+       [
+          'iv' => 'generated initialization vector (iv)',
+          'value' => 'encrypted, base64ed, signed value',
+          'mac'  => 'message authentication code (mac)'
+       ]
+
+    The 'value' is signed using a message authentication code (MAC) so verify that the value has not changed during
+    transit.
+
+    Parameters:
+    payload (str): Laravel encrypted text.
+    key (str): Encryption key (base64 decoded). Make sure 'base64:' has been removed from string.
+
+    Returns:
+    str: plaintext
+    """
+
     data = json.loads(base64.b64decode(payload))
-    key=os.environ['APP_KEY']
     if not valid_mac(key, data):
         return None
 
-    value =  base64.b64decode(data['value'])
+    value = base64.b64decode(data['value'])
     iv = base64.b64decode(data['iv'])
 
-    return unserialize(mcrypt_decrypt(value, iv, key))
+    return unserialize(mcrypt_decrypt(value, iv, key)).decode("utf-8")
 
 def mcrypt_decrypt(value, iv, key):
     AES.key_size=128
-
     crypt_object=AES.new(key=key,mode=AES.MODE_CBC,IV=iv)
     return crypt_object.decrypt(value)
 
 def unserialize(serialized):
     return loads(serialized)
 
-def valid_mac(key, payload):
+def valid_mac(key, data):
     dig = hmac.new(key, digestmod=hashlib.sha256)
     dig.update(data['iv'].encode('utf8'))
     dig.update(data['value'].encode('utf8'))
     dig = dig.hexdigest()
-    return dig==payload['mac']
+    return dig==data['mac']
